ad811378ec15241ed85825fbfb094a801d3f1251
joselabaki <jose.labaki@hotmail.com>
2021-03-25 19:30:55 +0200

    No change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-      root 'landing#index'
-      post 'emailapi/subscribe' => 'emailapi#subscribe'
+  root 'landing#index'
+
+  post 'emailapi/subscribe' => 'emailapi#subscribe'
+
+
+
+
+
+
 
 end
