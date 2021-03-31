caab7660b1ece300831053d30c3c54dfa6907079
joselabaki <jose.labaki@hotmail.com>
2021-03-31 17:19:36 +0300

    Phone Integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+before_action :detect_device_variant
+
+
+private
+
+def detect_device_variant
+request.variant = :phone if browser.device.mobile?
+end
+
+
+
 end
